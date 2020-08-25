ee3eb16b303480588ce587f07a7fc64601ee69a2
Duvier de Jesús Bohorquez Palacio <69058032+DuvierJBP@users.noreply.github.com>
2020-08-25 15:06:44 -0500

    NodeJS y Express
    Este proyecto consiste en una calculadora para sumar numeros y una calculadora de BMI, y así
    poner en práctica la codificación de un servidor local con la ayuda de NodeJS y Express.

---
+++ README.txt
@@ -0,0 +1,3 @@
+Esta práctica esta basada en la sección 18.
+Este proyecto consiste en una calculadora para sumar numeros y una calculadora de BMI, y así
+poner en práctica la codificación de un servidor local con la ayuda de NodeJS y Express.
---
+++ calculator.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/", function (req, res) {
+  console.log(req.body);
+
+  const num1 = req.body.number1;
+  const num2 = req.body.number2;
+
+  const result = Number(num1) + Number(num2);
+
+  res.send("the add between " + num1 + " and " + num2 + " is: " + result);
+});
+
+app.post("/bmicalculator", function (req, res) {
+  const weight = Number(req.body.weight);
+  const height = Number(req.body.height);
+
+  const result = Math.round(weight / Math.pow(height, 2));
+
+  res.send("Your BMI is: " + result);
+});
+
+app.listen(3000, function () {
+  console.log("The server is running in port 3000");
+});
